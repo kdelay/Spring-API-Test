bccf0af2257d4bf6f72ab5848c6d838effbee4a9
kdelay <kdelay20@gmail.com>
2024-06-12 17:44:23 +0900

    chore: dependency - swagger ui(springdoc)

---
+++ src/main/java/report/post/Configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package report.post.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Posting REST API Specifications")
+                        .description("게시글 CRUD")
+                        .version("v0.0.1"))
+                .components(new Components());
+    }
+}
