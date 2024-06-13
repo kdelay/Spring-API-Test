72651feafff59290e881588ec45582563c9c43fd
kdelay <kdelay20@gmail.com>
2024-06-13 23:36:36 +0900

    refactor: #2 service 리팩토링

--- src/main/java/report/post/service/PostService.java
+++ src/main/java/report/post/service/PostService.java
@@ -29,37 +29,44 @@ public class PostService {
 
     @Transactional
     public PostSelectResDTO select(Long postId) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("선택한 게시글이 없습니다."));
+        Post post = getPostById(postId);
         return new PostSelectResDTO(post.getTitle(), post.getClientName(), post.getCreateDate(), post.getContents());
     }
 
     @Transactional
     public Post update(Long postId, PostUpdateReqDTO dto) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("선택한 게시글이 없습니다."));
-        //비밀번호가 동일한지 검증
-        if (post.getPassword().equals(dto.getPassword())) {
-            //동일한 경우 수정 가능
+        Post post = getPostById(postId);
+
+        if (isEqualPassword(post, dto.getPassword())) {
             post.setClientName(dto.getClientName());
             post.setTitle(dto.getTitle());
             post.setContents(dto.getContents());
-            return post;
-        } else throw new IllegalArgumentException("비밀번호가 틀립니다.");
+        }
+        return post;
     }
 
     @Transactional
     public String delete(Long postId, String password) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("선택한 게시글이 없습니다."));
-        //비밀번호가 동일한지 검증
-        if (post.getPassword().equals(password)) {
+        Post post = getPostById(postId);
+
+        if (isEqualPassword(post, password)) {
             postRepository.deleteById(postId);
-        } else throw new IllegalArgumentException("비밀번호가 틀립니다.");
-        postRepository.flush();
+            postRepository.flush();
+        }
 
         if (postRepository.findById(postId).isEmpty()) {
             return "게시글을 삭제했습니다.";
         } else return "게시글 삭제 실패";
+    }
+
+    private Post getPostById(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("선택한 게시글이 없습니다."));
+    }
+
+    private boolean isEqualPassword(Post post, String password) {
+        if (post.getPassword().equals(password)) {
+            return true;
+        } else throw new IllegalArgumentException("비밀번호가 틀립니다.");
     }
 }
